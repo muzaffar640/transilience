1faa841f4c1d3f9dcac4215d10d41c2b1486953d
muzaffar640 <muzaffar640@gmail.com>
2024-05-26 16:23:54 +0530

    Add environment variable to fox deployment issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import { VulnerabilityData } from "../../types/VulnerabilityData";
 import ClientDashboard from "../components/ClientDashboard";
 
 export default async function HomePage() {
-  const res = await fetch("http://localhost:3001/api/vendorsData");
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL || ""}/api/vendorsData`
+  );
   const data: VulnerabilityData[] = await res.json();
 
   return (
